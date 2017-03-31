92f4c278792e404b21bc61f79b14ee07e676b8e2
Nitin kumar <knitin917@gmail.com>
2017-03-31 19:40:24 +0530

    Update admin.py

--- Airline/admin.py
+++ Airline/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Passenger,Flights,Aircraft,OperatedBy,Ticket,Booking,IssuedFor
+from .models import Passenger,Flights,Aircraft,OperatedBy,Ticket,Booking,IssuedFor
 # Register your models here.
 admin.site.register(Passenger)
 admin.site.register(Flights)
